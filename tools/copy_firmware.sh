690d4b68878f9820283ef1283c66879eb9df0fa1
Gurkengewuerz <niklas@schuetrumpf.net>
2023-10-01 19:33:50 +0200

    feat: also release apj

--- tools/copy_firmware.sh
+++ tools/copy_firmware.sh
@@ -9,9 +9,9 @@ src_dir="build/"
 rm -rf $dest_dir
 mkdir -p $dest_dir
 
-find "$src_dir" -type f -name "*.hex" -print0 | while IFS= read -r -d $'\0' file; do
+cp_file () {
     # Extract parent directory name
-    parent_dir=$(dirname "$file")
+    parent_dir=$(dirname "$1")
     grantparent_dir=$(dirname "$parent_dir")
     target_name=$(basename "$grantparent_dir")
 
@@ -21,4 +21,12 @@ find "$src_dir" -type f -name "*.hex" -print0 | while IFS= read -r -d $'\0' file
 
     # Copy the file to the destination directory
     cp "$file" "$dest_subdir/$(basename "$file")"
+}
+
+find "$src_dir" -type f -name "*.hex" -print0 | while IFS= read -r -d $'\0' file; do
+    cp_file $file
+done
+
+find "$src_dir" -type f -name "*.apj" -print0 | while IFS= read -r -d $'\0' file; do
+    cp_file $file
 done
