067e3a4e94472daf4e2f2a83032e2368cfcff0f4
Gurkengewuerz <niklas@schuetrumpf.net>
2023-10-01 18:39:21 +0200

    fix: test for number

--- tools/append_secure.sh
+++ tools/append_secure.sh
@@ -3,18 +3,21 @@
 # Set the directory path where "hwdef.dat" files are located
 target_folder="customTargets/"
 
+# Regular expression to match a number (integer or float)
+number_regex='^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$'
+
 # Loop through all environment variables starting with SECURE_
 for var in $(env | grep '^SECURE_'); do
     # Get the variable name and value
     var_name_without_prefix=$(echo $var | cut -d= -f1 | sed 's/^SECURE_//')
     echo "Found Variable $var_name_without_prefix"
     var_value=$(echo $var | cut -d= -f2)
-    if [[ $var_value == *" "* || -z $var_value ]]; then
-        # Variable value is a string, add double quotes
-        value_to_append="\"$var_value\""
-    else
-        # Variable value is not a string, no need to modify
+    if [[ $var_value =~ $number_regex ]]; then
+        # Variable value is a number, no need to add quotes
         value_to_append="$var_value"
+    else
+        # Variable value is not a number, add double quotes
+        value_to_append="\"$var_value\""
     fi
 
     value_to_append_escaped=$(echo "$value_to_append" | sed 's/"/\\\"/g')
